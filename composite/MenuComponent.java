f40393ee963950235d4352a69f0818fc553c1cc6
daeken.kwon <daeken.kwon@gmail.com>
2020-08-15 09:33:21 +0900

    add composite patten

---
+++ composite/MenuComponent.java
@@ -0,0 +1,34 @@
+package composite;
+
+public abstract class MenuComponent {
+    // For Composite Pattern
+	public void add(MenuComponent menuComponent) {
+		throw new UnsupportedOperationException();
+	}
+	public void remove(MenuComponent menuComponent) {
+		throw new UnsupportedOperationException();
+	}
+	public MenuComponent getChild(int i) {
+		throw new UnsupportedOperationException();
+	}
+  
+    // For Pizza
+	public String getName() {
+		throw new UnsupportedOperationException();
+	}
+	public String getDescription() {
+		throw new UnsupportedOperationException();
+	}
+	public double getPrice() {
+		throw new UnsupportedOperationException();
+	}
+	public boolean isVegetarian() {
+		throw new UnsupportedOperationException();
+	}
+
+	// public abstract Iterator<MenuComponent> createIterator();
+
+	public void print() {
+		throw new UnsupportedOperationException();
+	}    
+}
