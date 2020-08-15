c2b7646d5d8c2340fd30160fdbe8ffa443687991
daeken.kwon <daeken.kwon@gmail.com>
2020-08-15 14:28:21 +0900

    fix bug

--- 4-Iterator Pattern/IteratorPatternDemo.java
+++ 4-Iterator Pattern/IteratorPatternDemo.java
@@ -4,10 +4,11 @@ import java.util.*;
 
 public class IteratorPatternDemo {
 	
-	public static void main(String[] args) {
-	      
-	      for(Iterator<String> iter = cmpnyRepository.createIterator(); iter.hasNext();){
-	          String name = (String)iter.next();
+	public static void main(final String[] args) {
+		CollectionofNames cmpnyRepository = new CollectionofNames();
+
+		for (final Iterator<String> iter = cmpnyRepository.createIterator(); iter.hasNext();) {
+			final String name = (String) iter.next();
 	          System.out.println("Name : " + name);
 	       } 	
 	      
