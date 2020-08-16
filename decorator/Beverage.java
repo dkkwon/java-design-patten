a3659d92b6aba75bc0c3b27b45838b8b7a074639
daeken.kwon <daeken.kwon@gmail.com>
2020-08-16 13:01:52 +0900

    add TODO

--- decorator/Beverage.java
+++ decorator/Beverage.java
@@ -3,6 +3,8 @@ package decorator;
 public abstract class Beverage {
 	String description = "Unknown Beverage";
   
+	// TODO: interface 를 이용한 decorator pattern 구현
+
 	public String getDescription() {
 		return description;
 	}
