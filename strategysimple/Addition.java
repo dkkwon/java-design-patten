d4fd8eee986145731638151f2c4e2146ffaa6502
daeken.kwon <daeken.kwon@gmail.com>
2020-08-16 13:54:40 +0900

    add simple strategy pattern

---
+++ strategysimple/Addition.java
@@ -0,0 +1,11 @@
+
+package strategysimple;
+
+public class Addition implements Strategy{
+
+	@Override
+	public float calculation(float a, float b) {
+		return a+b;
+	}
+
+}
---
+++ strategysimple/Strategy.java
@@ -0,0 +1,7 @@
+package strategysimple;
+
+public interface Strategy {
+	
+	public float calculation(float a, float b);
+
+}
