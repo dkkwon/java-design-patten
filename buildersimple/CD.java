5119f1f213a8adc3151ca0a75d1988245b8b11c0
daeken.kwon <daeken.kwon@gmail.com>
2020-08-17 22:20:01 +0900

    add builder pattern (simple example)

---
+++ buildersimple/CD.java
@@ -0,0 +1,7 @@
+package buildersimple;
+
+public abstract class CD implements Packing {
+    public abstract String pack();
+
+    public abstract int price();
+}// End of the CD class.
---
+++ buildersimple/Company.java
@@ -0,0 +1,7 @@
+package buildersimple;
+
+public abstract class Company extends CD {
+   public abstract String pack();
+
+   public abstract int price();
+}// End of the Company class.
