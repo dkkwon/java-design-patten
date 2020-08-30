d84aba34a94fd298de3b31e6c05586d33c39d998
daeken.kwon <daeken.kwon@gmail.com>
2020-08-30 19:44:29 +0900

    add command simple remote control

---
+++ commandsimpleremote/Command.java
@@ -0,0 +1,5 @@
+package commandsimpleremote;
+
+public interface Command {
+	public void execute();
+}
---
+++ commandsimpleremote/GarageDoorOpenCommand.java
@@ -0,0 +1,13 @@
+package commandsimpleremote;
+
+public class GarageDoorOpenCommand implements Command {
+	GarageDoor garageDoor;
+
+	public GarageDoorOpenCommand(GarageDoor garageDoor) {
+		this.garageDoor = garageDoor;
+	}
+
+	public void execute() {
+		garageDoor.up();
+	}
+}
